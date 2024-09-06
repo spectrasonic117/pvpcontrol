c87ac37d36b8a051af1a312347c070ae04679bc7
Manuel Heredia <Spectrasonic117@gmail.com>
2024-09-05 22:00:12 -0600

    refactorizacion de codigo

--- src/main/java/com/spectrasonic/pvpcontrol/Main.java
+++ src/main/java/com/spectrasonic/pvpcontrol/Main.java
@@ -4,33 +4,31 @@ import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
-
+	private final String divider = "===============================";
+	private final String prefix = ChatColor.RED + "[" + ChatColor.GOLD + "PVPControl" + ChatColor.RED + "] " + ChatColor.RESET;
+	
     private PVPControl pvpControl;
 
     @Override
     public void onEnable() {
-        // Crear una instancia de la clase PVPControl
         pvpControl = new PVPControl(this);
 
-        // Registrar el comando /pvp
         getCommand("pvpcontrol").setExecutor(pvpControl);
 
-        // Mensaje en color verde cuando el plugin se activa
-        getServer().getConsoleSender().sendMessage("===============================");
+        getServer().getConsoleSender().sendMessage(divider);
         getServer().getConsoleSender().sendMessage(" ");
         getServer().getConsoleSender().sendMessage(ChatColor.RED + "Spectrasonic Plugins");
-        getServer().getConsoleSender().sendMessage(ChatColor.GOLD + "[ PVPControl ] " + ChatColor.GREEN + "Plugin Activado");
+        getServer().getConsoleSender().sendMessage(prefix + ChatColor.GREEN + "Plugin Activado");
         getServer().getConsoleSender().sendMessage(" ");
-        getServer().getConsoleSender().sendMessage("===============================");
+        getServer().getConsoleSender().sendMessage(divider);
     }
 
     @Override
     public void onDisable() {
-        // Mensaje en color rojo cuando el plugin se desactiva
-        getServer().getConsoleSender().sendMessage("===============================");
+        getServer().getConsoleSender().sendMessage(divider);
         getServer().getConsoleSender().sendMessage(" ");
-        getServer().getConsoleSender().sendMessage(ChatColor.GOLD + "[ PVPControl ] " + ChatColor.RED + "Plugin Desactivado");
+        getServer().getConsoleSender().sendMessage(prefix + ChatColor.RED + "Plugin Desactivado");
         getServer().getConsoleSender().sendMessage(" ");
-        getServer().getConsoleSender().sendMessage("===============================");
+		getServer().getConsoleSender().sendMessage(divider);
     }
 }
