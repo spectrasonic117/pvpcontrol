6b581f6b69f99117b6c9583938d5827c01917512
Manuel Heredia <Spectrasonic117@gmail.com>
2024-09-06 13:17:21 -0600

    1.7 version refactoring code

--- src/main/java/com/spectrasonic/pvpcontrol/PVPControl.java
+++ src/main/java/com/spectrasonic/pvpcontrol/PVPControl.java
@@ -5,19 +5,26 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class PVPControl implements CommandExecutor {
 
 	private final String prefix = ChatColor.RED + "[" + ChatColor.GOLD + "PVPControl" + ChatColor.RED + "] " + ChatColor.RESET;
-    private boolean pvpEnabled = true;
+    private boolean pvpEnabled;
 	private final Main plugin;
-	private final String version = "1.6";
+	private final String version = "1.7";
 
     public PVPControl(Main plugin) {
         this.plugin = plugin;
     }
     @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		
+		if (sender instanceof Player && !sender.isOp()) {
+			sender.sendMessage(prefix + ChatColor.RED + "No tienes permisos para ejecutar este comando");
+			return true;
+		}
+
         if (command.getName().equalsIgnoreCase("pvpcontrol")) {
             if (args.length == 1) {
                 if (args[0].equalsIgnoreCase("on")) {
@@ -30,7 +37,6 @@ public class PVPControl implements CommandExecutor {
                     sender.sendMessage(prefix + "PvP " + ChatColor.RED + "Desactivado");
                 } else if (args[0].equalsIgnoreCase("version")) {
                     pvpEnabled = false;
-                    Bukkit.getWorlds().forEach(world -> world.setPVP(false));
                     sender.sendMessage(prefix + "Version: " + ChatColor.GOLD + version);
                     sender.sendMessage(prefix + "Developed by " + ChatColor.RED + "Spectrasonic");
                 } else {
